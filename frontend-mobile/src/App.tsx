870fb4eec9e1d0db391000ebcc99c48ddc0a7ffb
elgar <juancarlosgarcia.an@gmail.com>
2025-12-03 19:47:52 +0100

    feat: created error-login page

--- frontend-mobile/src/App.tsx
+++ frontend-mobile/src/App.tsx
@@ -5,6 +5,7 @@ import { IonReactRouter } from '@ionic/react-router';
 /* Theme variables */
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import ErrorLogin from './pages/ErrorLogin';
 import Home from './pages/Home';
 
 
@@ -45,6 +46,7 @@ const App: React.FC = () => (
       <IonRouterOutlet>
         <Route path="/login" component={Login} exact />
         <Route path="/signup" component={Signup} exact />
+        <Route path="/error-login" component={ErrorLogin} exact />
         <Route path="/home" component={Home} exact />
         <Redirect exact from="/" to="/login" />
       </IonRouterOutlet>
